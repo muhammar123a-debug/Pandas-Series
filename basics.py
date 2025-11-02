8abb9158e024bbc5799434878a779f8394d691ad
muhammar123a-debug <muhammar123a@gmail.com>
2025-11-02 20:26:13 +0500

    add code

--- basics.py
+++ basics.py
@@ -1,2 +1,14 @@
 import pandas as pd
 
+#read datasets from CSV file in dataframe
+df = pd.read_csv("sales_data_sample.csv",encoding="latin1")
+
+
+#ROWS (head , tails)
+# print("This is Head")
+# print(df.head(3))
+# print("This is Tail")
+# print(df.tail(3))
+
+print("Displaying the info of data sets")
+print(df.info())
